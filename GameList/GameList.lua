a9115866c7847347f1ec8f2d4a239fce90b93b3f
naruto9161 <49629118+naruto9161@users.noreply.github.com>
2020-04-08 15:02:22 +0700

    Update GameList.lua

--- GameList/GameList.lua
+++ GameList/GameList.lua
@@ -1,3 +1,3 @@
 local GamesList ={
-    ["4520536611"] = {4520536611,"Fitness-Simulator","https://raw.githubusercontent.com/naruto9161/S1mulat0r-Hub/master/scripts/fitness_sim.lua"}
+    ["4520536611"] = {"Fitness-Simulator","https://raw.githubusercontent.com/naruto9161/S1mulat0r-Hub/master/scripts/fitness_sim.lua"}
 }
